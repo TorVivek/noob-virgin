903bd6da30976380090a2a655002d6cf716e4596
Vivek-noob <67530839+TorVivek@users.noreply.github.com>
2021-01-04 20:08:41 +0530

    Add files via upload
    For the sake of virginity do not execute the code....
    If then also you do so it's your fault :)

---
+++ virgin_noob.py
@@ -0,0 +1,12 @@
+"""
+Being a virgin noob for the sake of Sam Miller
+"""
+
+import webbrowser,random
+id = ['3rvwRig','2KRCrKW','2KTQsHZ']
+pre = "https://bit.ly/"
+print("HELLO WORLD\n")
+print ("GO ON FUCKING VIRGIN MASTERBATER PROGRAMMER!!!")
+webbrowser.open(pre+random.choice(id))
+
+#Thanks buddy to give us this shit opportunity :)
